59be0c2b8ad3bda09fe971b774e9f177b76e46ff
Laurent Schwitter <laurent.schwitter@gmail.com>
2023-02-15 12:27:48 +0100

    angular environments

---
+++ frontend/src/environment/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  serverURL: `https://eltharynd.com/`,
+
+  apiBASE: `api/v1/`,
+  socketBASE: `api/socket.io`,
+};
---
+++ frontend/src/environment/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  serverURL: `http://localhost:3000/`,
+
+  apiBASE: `api/v1/`,
+  socketBASE: `api/socket.io`,
+};
